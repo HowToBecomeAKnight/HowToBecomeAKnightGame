1eb3470e0873b4631bbafd82ce6617d17a93657e
Jake Moore <jake.moorevr@gmail.com>
2016-10-29 15:46:24 -0700

    no message

---
+++ Assets/Scripts/LoadForestHub.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadForestHub : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start() { }
+
+    // Update is called once per frame
+    void Update() { }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Player hit portal");
+
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Load Foresthub");
+            Application.LoadLevel("ForestHub");
+        }
+    }
+}
