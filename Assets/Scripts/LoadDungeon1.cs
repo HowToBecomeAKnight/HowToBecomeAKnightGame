c14a74376fabe79b91fa2390984c4e57841b0af8
Jake Moore <jake.moorevr@gmail.com>
2016-10-29 14:06:45 -0700

    Added portal that takes you to the first dungeon, also created the first dungeon cave and added the character and some rocks to it.

---
+++ Assets/Scripts/LoadDungeon1.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadDungeon1 : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start() { }
+
+    // Update is called once per frame
+    void Update() { }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Player hit portal1");
+
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Load dungeon1");
+            Application.LoadLevel("dungeon1");
+        }
+    }
+}
