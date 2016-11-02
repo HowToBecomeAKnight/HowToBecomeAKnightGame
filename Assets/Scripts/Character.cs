a2ebebf084e378e0516e7925eae76da71d0518c3
Blair <Blair_Elliott@live.ca>
2016-11-02 04:15:02 -0300

    - add start menu - convert character to a prefab - add character UI and script to handle character stats

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class Character : MonoBehaviour {
+
+    public Image HealthBar;
+
+	// Use this for initialization
+	void Start () {
+	    HealthBar.fillAmount = 1f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    public void RemoveHealth(int amount)
+    {
+        //TODO: do some sort of animation
+        HealthBar.fillAmount -= amount;
+    }
+
+    public void AddHealth(int amount)
+    {
+        //TODO: do some sort of animation
+        HealthBar.fillAmount += amount;
+    }
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject character;
+    public Canvas characterUI;
+    public Button startText;
+    public Button quitText;
+
+    // Use this for initialization
+    void Start()
+    {
+        characterUI.enabled = false;
+        startText = startText.GetComponent<Button>();
+        quitText = quitText.GetComponent<Button>();
+        character.GetComponent<NormalMovement>().enabled = false;
+        character.GetComponent<Animations>().enabled = false;
+        
+
+    }
+
+    public void onStartPressed()
+    {
+        Destroy(startText.gameObject);
+        Destroy(quitText.gameObject);
+        characterUI.enabled = true;
+        character.GetComponent<NormalMovement>().enabled = true;
+        character.GetComponent<Animations>().enabled = true;
+    }
+
+    public void onExitGame()
+    {
+        Application.Quit();
+    }
+}
