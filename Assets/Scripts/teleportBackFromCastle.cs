ae53e35e5b50e734d65e269a31e61debfa1513a6
Jake Moore <jake.moorevr@gmail.com>
2016-11-12 16:00:02 -0800

    All portals in dungeon2 are up and running, and I also added 6 animations to the character hands, which can be found as 'hands1' in the character folder.

---
+++ Assets/Scripts/teleportBackFromCastle.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class teleportBackFromCastle : MonoBehaviour {
+
+    public GameObject character;
+    public GameObject teleportToHere;
+
+    // Use this for initialization
+    void Start() { }
+
+    // Update is called once per frame
+    void Update() { }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Player hit BackFromCastle teleporter");
+
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("teleport to ToCastle portal");
+            character.transform.position = teleportToHere.transform.position;
+        }
+    }
+}
---
+++ Assets/Scripts/teleportToCastle.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class teleportToCastle : MonoBehaviour {
+
+    public GameObject character;
+    public GameObject teleportToHere;
+
+    // Use this for initialization
+    void Start() { }
+
+    // Update is called once per frame
+    void Update() { }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Player hit ToCastle teleporter");
+
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("teleport to castle");
+            character.transform.position = teleportToHere.transform.position;
+        }
+    }
+}
