c76065e4d3744201eaf828e3a0dd4b9400030e14
Jake Moore <jake.moorevr@gmail.com>
2016-11-15 15:57:40 -0800

    Remade character hands and setup animator, as well as made all animations running smoothly.

--- Assets/Scripts/Animations.cs
+++ Assets/Scripts/Animations.cs
@@ -5,13 +5,18 @@ public class Animations : MonoBehaviour
 {
 
     Animator anim;
-
+    private CharacterController controller;
     BoxCollider collider;
+    private bool onGround;
 
     // Use this for initialization
     void Start()
     {
+        //get animator on character
         anim = GetComponent<Animator>();
+        //get controller on character
+        controller = GetComponent<CharacterController>();
+
         var weapon = GameObject.FindWithTag("Weapon");
         collider = weapon.GetComponent<BoxCollider>();
         //Disable the collider on the weapon untill the player swings it
@@ -22,25 +27,62 @@ public class Animations : MonoBehaviour
     void Update()
     {
 
-        if (Input.GetMouseButtonDown(0))// 
+        //Left click attack, slice attack
+        if (Input.GetMouseButtonDown(0)) 
         {
             collider.isTrigger = false;
-            anim.SetTrigger("Attack");
-            Debug.Log("Attack");
+            anim.SetTrigger("Slice Attack");
+            Debug.Log("Slice Attack");
         }
-        //For Keyboard --------------------------
+        
+        //right click stab attack
+        else if (Input.GetMouseButtonDown(1))
+        {
+            collider.isTrigger = false;
+            anim.SetTrigger("Stab Attack");
+            Debug.Log("Stab Attack");
+        }
+       
+        //Set speed to 1.0 when W is pressed
         if (Input.GetKey(KeyCode.W))//W
         {
             anim.SetFloat("Speed", 1);
         }
+        //Set speed to -1 when S is pressed 
+        else if (Input.GetKey(KeyCode.S))
+        {
+            anim.SetFloat("Speed", -1);
+        }
+        //When at 0, play Idle animation
         else
             anim.SetFloat("Speed", 0);
 
+        //play the jump animation when space is pressed, only trigger when player is grounded
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            anim.SetTrigger("Jump");
-            Debug.Log("Jump");
+            if (onGround)
+            {
+                anim.SetTrigger("Jump");
+                Debug.Log("Jump");
+            }
         }
+
+        //to check if character is grounded
+        if (controller.isGrounded)
+        {
+            onGround = true;
+        }
+        else
+        {
+            onGround = false;
+        }
+
+        //pull lever if standing still
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            anim.SetTrigger("Pull Lever");
+        }
+        
     }
 
     public void AttackDone()
