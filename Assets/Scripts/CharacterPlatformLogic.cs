0b091a7c3b180675a643f05ee728bfbefe40ef8d
Jennifer Terpstra <jenniferlynnterpstra@gmail.com>
2016-11-19 22:13:07 -0400

    Added moving platform into dungeon 1

---
+++ Assets/Scripts/CharacterPlatformLogic.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*Makes the character a child of the moving platform so that it does not move in strange ways as the platform moves*/
+public class CharacterPlatformLogic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //Make player child of platform when they are on it
+    void OnTriggerEnter(Collider other)
+    {
+        other.transform.parent = gameObject.transform;
+    }
+
+    //Player is no longer child of plaform when they get off it
+    void OnTriggerExit(Collider other)
+    {
+        other.transform.parent = null;
+    }
+}
